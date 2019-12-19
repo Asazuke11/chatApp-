83562136f029bd093da3f64a522d04ac1129696f
Asazuke <quusan3@gmail.com>
2019-12-19 14:12:38 +0900

    暫定的にconfig

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  Pass_Add: {
+    Git_Id : "5a790a788dcf2136d3a2",
+    Git_Sec : "a7b1d606fd6587adb0972d28ea3412d6676c99da",
+    session_Secret: "87c4c504b2c72405",
+    Twi_Id:"C2TJCPcAc5ZnEa35HhJaDQXgW",
+    Twi_Sec:"iZ1kKTAf3Rdb5SWmfN7sfQSYTkFzWR6Au1xn7vjAHqJpAT3BNC"
+  },
+  this_pages_Address: 'http://localhost:8000',
+  Git_collback_Address:"http://localhost:8000/auth/github/callback",
+  Twi_collback_Address:"http://localhost:8000/auth/twitter/callback"
+};
