60fcd1be5152fadfa15712115c497935cd8f4a33
Asazuke <quusan3@gmail.com>
2021-11-20 16:50:15 +0900

    Heroku対応

--- models/sequelize-loader.js
+++ models/sequelize-loader.js
@@ -1,10 +1,30 @@
 'use strict';
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost/wintern',{
-    operatorsAliases:false
+
+const dialectOptions = {
+  ssl: {
+    require: true,
+    rejectUnauthorized: false
   }
-);
+};
+
+const sequelize = process.env.DATABASE_URL ?
+  // 本番環境
+  new Sequelize(
+    process.env.DATABASE_URL,
+    {
+      logging: false,
+      dialectOptions
+    }
+  )
+  :
+  // 開発環境
+  new Sequelize(
+      'postgres://postgres:postgres@localhost/wintern',
+    {
+      logging: false
+    }
+  );
 
 module.exports = {
   database: sequelize,
